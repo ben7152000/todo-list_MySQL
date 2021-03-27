3462e54acc01cdaf93701e976bdd67ecbee116df
Ben Chou <ben7152000@gmail.com>
2021-03-28 06:53:02 +0800

    schema setting

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    static associate (models) {
+      Todo.belongsTo(models.User)
+    }
+  };
+  Todo.init({
+    name: DataTypes.STRING,
+    isDone: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Todo'
+  })
+  return Todo
+}
