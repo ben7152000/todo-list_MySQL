b808d35aaa7ee2ea446f668f85a0ca4ffae67fce
Ben Chou <ben7152000@gmail.com>
2021-03-28 06:32:35 +0800

    init project

---
+++ app.js
@@ -0,0 +1,27 @@
+// define modules
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+// setting port
+const app = express()
+const PORT = 3000
+
+// setting body-parser
+app.use((bodyParser.urlencoded({ extended: false })))
+app.use(bodyParser.json())
+
+// setting express-handlebars
+app.set('view engine', 'handlebars')
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.use(express.static('public'))
+
+// setting router
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// listen server
+app.listen(PORT, () => {
+  console.log(`The server is working on the localhost:${PORT}`)
+})
