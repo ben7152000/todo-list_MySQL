4fe544be615dd7b5f12181d70c15cd51d605630a
Ben Chou <ben7152000@gmail.com>
2021-03-28 16:33:59 +0800

    finish logout feature

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,15 @@
+const db = require('../models')
+const Todo = db.Todo
+
 const todoController = {
-  getTodo: (req, res) => {
-    res.render('../views/todo')
+  getTodo: async (req, res) => {
+    const UserId = req.user.id
+    try {
+      const todo = await Todo.findAll({ raw: true, nest: true, where: { UserId } })
+      res.render('../views/todo', { todo })
+    } catch (e) {
+      console.log(e)
+    }
   },
   createTodoPage: (req, res) => {
     res.send('create')
